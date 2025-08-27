27bf36df844acd51dd645611571963b9cd8b2c76
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-08-27 08:41:36 +0530

    TOday I Have Learn Abut a Comments and escap and seprate comments in single stsment and New Line Concept

---
+++ Day_1_to_10/Day_5.py
@@ -0,0 +1,27 @@
+'''
+sldknflsnl
+fsmdf
+sfnlsdffdkn
+'''
+#also 3 double coute
+# dcsf
+# sdfsd
+# fdsfsd
+# sdf
+#Hello this is Comments 
+print("Heello Day 5")
+
+print("Hello aman \n" \
+" ok this is new line dlknfsdnf") #srfnlkrsnfn
+
+
+# print("Hello World")
+
+
+
+
+
+print("aman \"hey aman\"\asdsf")
+print("aman",1,2,3)
+
+print("Hey",6,7,sep="~",end="009\n")
