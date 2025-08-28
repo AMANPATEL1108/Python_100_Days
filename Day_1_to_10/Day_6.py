2f37f50e74763dfb21786478f7b8dd601a6c859d
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-08-28 21:38:17 +0530

    Today is Day 6 Learn About DataTypes and Tuple,Dictonary and list

---
+++ Day_1_to_10/Day_6.py
@@ -0,0 +1,37 @@
+a=1
+print(type(a))
+
+b=input("ENter a Data")
+print(type(b))
+
+p1=5
+p2=7
+print(p1+p2)
+
+
+c=True
+d=None
+e=1.1
+print("Type of p1 ",type(p1))
+print("Type of  b",type(b))
+print("Type of c ",type(c))
+print("Type of d ",type(d))
+print("Type of e ",type(e))
+
+comNumber=complex(8,3)
+print(comNumber)
+print(type(comNumber))
+
+
+#change we can list
+list1=[8,5,4,[-1,5],["aman","dev"]]
+print(list1)
+
+
+#tuple can not chnage we can 
+tuple1=(("aman","dev"),("asd","dsa"))
+print(tuple1)
+
+#Dictonary (Map Datta Type ,Collection of Key Value Pairs)
+dict1={"name":"aman","age":21,"canVote":True}
+print(dict1)
