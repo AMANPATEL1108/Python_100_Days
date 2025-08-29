74b9c5c314b97c0d2e8feb425ef86f203816db50
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-08-29 08:44:25 +0530

    Today is Day 7 i have completed a Oprators and create a Basic Calculator Using a Python

---
+++ Day_1_to_10/Day_7.py
@@ -0,0 +1,40 @@
+# print(5+7)
+# print(5-2)
+# print(15*7)
+# print(12/2)
+# print(12//6)
+# print(5%3)
+# print(5**3)
+
+
+# Initialize variables a and b
+a = 10
+b = 5
+
+# Print sum
+print("Sum:")
+print(a + b)
+
+# Print subtraction
+print("Subtraction:")
+print(a - b)
+
+# Print multiplication
+print("Multiplication:")
+print(a * b)
+
+# Print division
+print("Division:")
+print(a / b)
+
+# Print floor division
+print("Floor Division:")
+print(a // b)
+
+# Print modulus (remainder)
+print("Modulus:")
+print(a % b)
+
+# Print exponentiation
+print("Exponentiation:")
+print(a ** b)
