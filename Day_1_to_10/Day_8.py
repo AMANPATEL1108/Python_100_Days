fc16bbfe41838306147aacb05cdae09acc103f32
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-08-30 09:39:03 +0530

    Today is Day 8 TOday Lean About a Solution of that Exercise

---
+++ Day_1_to_10/Day_8.py
@@ -0,0 +1,32 @@
+
+# Initialize variables a and b
+a = 10
+b = 5
+
+# Print sum
+print("Sum:")
+print(a + b)
+
+# Print subtraction
+print("Subtraction:")
+print(a - b)
+
+# Print multiplication
+print("Multiplication:")
+print(a * b)
+
+# Print division
+print("Division:")
+print(a / b)
+
+# Print floor division
+print("Floor Division:")
+print(a // b)
+
+# Print modulus (remainder)
+print("Modulus:")
+print(a % b)
+
+# Print exponentiation
+print("Exponentiation:")
+print(a ** b)
