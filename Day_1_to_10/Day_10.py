a5e93f4a5af8f0704c1e71146492f68abb8bf051
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-09-01 08:48:38 +0530

    Today is Day 10 I have Learn About How to Take a Input ini a Python as a valid Type

---
+++ Day_1_to_10/Day_10.py
@@ -0,0 +1,13 @@
+a=input("Enter Your Name:")
+print("Your Name is ",a)
+
+x=int(input("Enter First Number:"))
+y=int(input("Enter Second Number:"))
+
+print(x+y)
+# print(int(x)+int(y))  #if not taken int COnvetr in input then 
+
+p=float(input("Enter Float Numebr 1:"))
+q=float(input("Enter Float Number 2:"))
+
+print(p+q)
