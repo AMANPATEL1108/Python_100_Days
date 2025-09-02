0a7525417ab871a8a98a369650cdc2dd148fd01b
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-09-02 07:55:37 +0530

    Today is Day 11 I have Learn About a String

---
+++ Day_11_to_20/Day_11.py
@@ -0,0 +1,20 @@
+name="aman"
+
+friend='dev'
+
+apple="he said , \"I want to eact an apple\""
+print(apple)
+
+
+appl2="""
+aman
+is here 
+ new line
+"""
+print(appl2)
+
+print(name[0:3])
+# print(name[10]) #Throws Error 
+print("Here is For Loops")
+for character in name:
+    print(character)
