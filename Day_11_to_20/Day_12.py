cf5da0bc163256ea39b2e8fe974d27da34cb5ea1
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-09-03 09:02:18 +0530

    Today is Day 12 I have Learn About a String Slicing and Negitive Slicing

---
+++ Day_11_to_20/Day_12.py
@@ -0,0 +1,18 @@
+name="amanpatel"
+
+print(name[len(name)-3:len(name)-1])  #This and Below Same
+print(name[-3:-1])  #This is and Above Same
+# print(name[0:4])
+
+# print(len(name))
+
+pi="Applee"
+
+print(pi[:len(pi)])  #or pi[:] same
+print(pi[5])
+
+#including 0 not len here len -1 include
+print(name[0:len(name)-3])  #or same  print(name[0:-3])
+
+a="aman"
+print(a[-4:-2])
