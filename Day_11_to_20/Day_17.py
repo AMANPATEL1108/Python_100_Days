4027afc386cb089b606da434ab6a7e7c02644868
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-09-08 08:28:51 +0530

    Today is Day 17 I have Learn About a Loops

---
+++ Day_11_to_20/Day_17.py
@@ -0,0 +1,21 @@
+# name="aman"
+# for i in name:
+#     print(i)
+#     if(i=='a'):
+#         print("This is a is special")
+
+# colors=["red","green","yellow"]
+
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# for k in range(5):
+#     print(k+1)
+
+# for k in range(1,11):
+    # print(k)
+
+for k in range(1,12,3):
+    print(k)
