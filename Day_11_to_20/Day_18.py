d8e4be462c77f48580d62ed31a801b29a4653889
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-09-09 09:02:55 +0530

    Today is Day 18 I have a Learn About a While Loops in Python

---
+++ Day_11_to_20/Day_18.py
@@ -0,0 +1,28 @@
+count=1
+
+
+while(count<10):
+    print(count)
+    count=count+1
+
+print("Done Loop")
+
+inp=int(input("Enter a Input"))
+
+while(inp<=38):
+    inp=int(input("Enter a Input"))
+    print(inp)
+
+print("Done Loop sec")
+
+
+while(count>0):
+    print(count>0)
+    count=count-1
+else:
+    print("Inside Else")
+
+#Do while Syntexx but not in python that 
+# do{
+#     #body
+# }while(condition)
