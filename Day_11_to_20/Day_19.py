989fd24d6ac735dbc4c5c7f048bbaf8cbdc247e0
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-09-10 08:46:02 +0530

    Today is Day 19 I Have Learn About a Continue and Break Statement in Python

---
+++ Day_11_to_20/Day_19.py
@@ -0,0 +1,15 @@
+# for i in range(12):
+#     if(i==10):
+#         print("Skip Iteration")
+#         # break; #Break Loop
+#         continue;
+#     print("5 X",i,"=",5*i)
+    
+# print("Exited")
+
+i=0
+while True:
+    print(i)
+    i=i+1;
+    if(i%100==0):
+        break;
