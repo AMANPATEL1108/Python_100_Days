b26672fcc337c0cec5fd1a60c5aea73372287d2e
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-09-11 08:52:54 +0530

    Today is Day 20 I have Learn About a Function in a Python

---
+++ Day_11_to_20/Day_20.py
@@ -0,0 +1,18 @@
+
+def calculateGmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+
+
+def check(a,b):
+    if(a>b):
+        print("First is greter")
+    else:
+        print("Second is Greter")
+    
+def isLesser(a,b):
+    pass      #Check next Program Not Writed righ now body
+a=9
+b=8
+calculateGmean(a,b)
+check(a,b)
