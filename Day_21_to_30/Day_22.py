19e4cd5cac7fcbcb282a7147d5d24f50009ff67a
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-09-13 10:34:40 +0530

    Today is Day 22 I have Learn Aboy a List and Minus INdexing and also how to store a value in a List and How to REad a Llist Values

---
+++ Day_21_to_30/Day_22.py
@@ -0,0 +1,21 @@
+list1=[1,2,3,4,5,6,7,8,9]
+# print(list) 
+
+# print(list1[-3])
+
+
+colors=["blue","Green","Red","Yellow"]
+
+if "Red" in colors:
+    print("Red is Present")
+else:
+    print("Red is Absent")
+
+    print(colors[1:-1])
+
+print(colors[::2])
+
+
+lst=[i for i in range(5) if i%2==0]
+
+print(lst)
