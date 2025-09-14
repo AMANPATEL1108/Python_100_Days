cb289b9d67dcc3a17419ec030b428ddd314f83b7
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-09-14 07:43:58 +0530

    Today is Day 23 I hav eLearn About a Methods of a List Completed

---
+++ Day_21_to_30/Day_23.py
@@ -0,0 +1,15 @@
+l=[1,22,3,5,7,8]
+
+# l.append(9)
+l.sort()
+l.reverse()
+print(l.index(1))
+print(l.count(22))
+# m=l
+# m[0]=0
+m=[11,12,134]
+k=l+m
+print(k)
+l.extend(m)
+l.insert(1,88)
+print(l)
