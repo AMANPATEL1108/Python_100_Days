0578cb79ea5709666cbc53ce950f93643908642d
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-10-04 09:25:59 +0530

    Today is Day 43 I have Learn About a virtual env in python

---
+++ Day_41_to_50/Day_43/Day_43.py
@@ -0,0 +1,10 @@
+
+#Create Env :python -m venv myenv
+# activate command for Env:.\myenv\Scripts\Activat
+#Deactivate for Command : deavtivate
+
+#for get all packages print : pip freeze 
+
+#for get all packages file txt create : pip freeze > requirement.txt 
+
+#intall a from txt file all packages for command : pip install -r requirement.txt
