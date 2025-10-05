1c056881a941f9d98acff77eb27709fed5b938f9
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-10-05 08:29:15 +0530

    Today is Day 44 I have Learn About a Import how to worke all things in that

---
+++ Day_41_to_50/Day_44/Day_44.py
@@ -0,0 +1,14 @@
+import math
+from math import sqrt as s,pi as p
+from math import *
+from Day_41_to_50.Day_44.module_Day_44 import welcome,aman
+
+print(math.floor(4.2343))
+print(s(12) * p)
+print(s(12)) # direct use after adding from 
+
+print(dir(math))
+print(math.nan,type(math.nan))
+
+welcome()
+print(aman)
