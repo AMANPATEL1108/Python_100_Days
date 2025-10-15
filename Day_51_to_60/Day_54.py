dd17e05e4e9ca10aaa014e620eb3dc45915d5f6a
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-10-15 08:51:04 +0530

    Today is Day 54 I have Learn About a is and == in python

---
+++ Day_51_to_60/Day_54.py
@@ -0,0 +1,8 @@
+# ==  (Compare value)
+# is (Compare to Memory Location)
+
+a=3
+b=3  #Pthong not create a immutable object for same memory location use
+
+print(a is b) # memory location
+print(a == b) #value compare
