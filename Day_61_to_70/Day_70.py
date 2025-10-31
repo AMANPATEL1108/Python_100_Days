01d6ce8828a98676e218dd0f97dff324379def62
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-10-31 08:25:29 +0530

    Today is Day 70 I hav Learn About a alternative Constructor

---
+++ Day_61_to_70/Day_70.py
@@ -0,0 +1,20 @@
+class Employee:
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+
+
+    @classmethod
+    def fromstr(cls,string):
+        return cls(string.split("-")[0],int(string.split("-")[1]))
+
+
+e=Employee("Asd",12000)
+print(e.name)
+print(e.salary)
+
+
+string="Asd-12000"
+e1=Employee.fromstr(string)
+print(e1.name)
+print(e1.salary)
